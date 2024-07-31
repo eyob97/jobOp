4707246806712907506e0672bb8c24734cd4383c
nazlicankurtben <nazlicankurtben@gmail.com>
2024-07-31 23:11:10 +0200

    Fix data return type

--- src/app/components/withAuth.tsx
+++ src/app/components/withAuth.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useEffect } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 
@@ -14,17 +14,17 @@ const withAuth = (
 ) => {
   const Wrapper = (props: any) => {
     const router = useRouter();
-    const { isAuthenticated, isLoading } = useSelector((state: RootState) => {
+    const { user, isLoading } = useSelector((state: RootState) => {
       console.log('Redux State:', state); 
       return state.auth;
     });
 
     useEffect(() => {
-      console.log('Auth Status:', { isAuthenticated, isLoading }); 
-      if (!isLoading && !isAuthenticated && !options?.allowUnauthenticatedAccess) {
+      console.log('Auth Status:', { user, isLoading }); 
+      if (!isLoading && !user && !options?.allowUnauthenticatedAccess) {
         router.replace("/auth/sign-in");
       }
-    }, [isAuthenticated, isLoading, router]);
+    }, [user, isLoading, router]);
 
     if (isLoading) {
       return <div>Loading...</div>;
