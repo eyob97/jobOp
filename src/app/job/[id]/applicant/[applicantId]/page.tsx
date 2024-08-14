f22c112c48c2950f8257099b27980e800f3160f7
1eyob <eyobbirhanu01@gmail.com>
2024-08-14 15:03:55 +0300

    ✨ feat: page

---
+++ src/app/job/[id]/applicant/[applicantId]/page.tsx
@@ -0,0 +1,12 @@
+import ApplicantProfile from "@/app/components/Employer/ApplicantDetails";
+import React from "react";
+
+export const ApplicantDetail = () => {
+  return (
+    <>
+      <ApplicantProfile />
+    </>
+  );
+};
+
+export default ApplicantDetail;
