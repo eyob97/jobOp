1c4aa4d22859163ddcdb667c4df68c12f2031014
1eyob <eyobbirhanu01@gmail.com>
2024-08-16 11:50:28 +0300

    🐞 fix(for a jobs list): filter and search

--- src/app/components/JobSeeker/FilterDashboard.tsx
+++ src/app/components/JobSeeker/FilterDashboard.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { useState } from "react";
 import JobCard from "./JobCard";
@@ -11,22 +11,45 @@ interface SearchCriteria {
 }
 
 const FilterDashboard = () => {
-  const [searchCriteria, setSearchCriteria] = useState<SearchCriteria>({ jobTitle: "", location: "", filter: "" });
+  const [searchCriteria, setSearchCriteria] = useState<SearchCriteria>({
+    jobTitle: "",
+    location: "",
+    filter: "",
+  });
 
   const handleSearch = (criteria: SearchCriteria) => {
-    setSearchCriteria(criteria);
+    if (!criteria.jobTitle && !criteria.location && !criteria.filter) {
+      setSearchCriteria({ jobTitle: "", location: "", filter: "" });
+    } else {
+      setSearchCriteria(criteria);
+    }
   };
 
+  const clearSearch = () => {
+    setSearchCriteria({ jobTitle: "", location: "", filter: "" });
+  };
   return (
     <>
-        <div className="w-full h-full p-4">
-      <SearchBar onSearch={handleSearch} />
-      <div className="max-w-7xl mx-auto p-4">
-      <h2 className="text-2xl font-bold mb-4">Jobs for you</h2>
-      <JobCard searchCriteria={searchCriteria} /></div>
-    </div>
-    </>
+      <div className="w-full h-full p-4">
+        <SearchBar onSearch={handleSearch} />
+        <div className="max-w-7xl mx-auto p-4">
+          {(searchCriteria.jobTitle ||
+            searchCriteria.location ||
+            searchCriteria.filter) && (
+            <button
+              className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600"
+              onClick={clearSearch}
+            >
+              Clear Filters
+            </button>
+          )}
 
+          <h2 className="text-2xl font-bold mb-4">Jobs for you</h2>
+
+          <JobCard searchCriteria={searchCriteria} />
+        </div>
+      </div>
+    </>
   );
 };
 
